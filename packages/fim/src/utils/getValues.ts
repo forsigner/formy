1b427a9ec147e0fb20e7287adf9cb763ec4dfc36
forsigner <forsigner@gmail.com>
2020-12-18 17:01:55 +0800

    feat: skip invisible field when getValues()

--- packages/fim/src/utils/getValues.ts
+++ packages/fim/src/utils/getValues.ts
@@ -6,9 +6,10 @@ export function getValues(formName: string): any {
   let values: any = {}
   const { stores } = Storage
   for (const key in stores) {
-    if (!key.startsWith(`${formName}-`)) continue
+    if (!key.startsWith(`${formName}-`)) continue // skip by formName
     const { state } = Storage.get<FieldState>(key)
-    if (Array.isArray(state)) continue
+    if (Array.isArray(state)) continue // skip FieldArray state
+    if (!state.visible) continue // skip invisible field
 
     const { name, value } = state
     set(values, name, value)
